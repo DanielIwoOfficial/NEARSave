a78d45c9412d7bee8ddc0ba3f98858b91a67db15
isdl <isdl@windows7.isdl>
2020-05-20 02:26:04 +0100

    NearSave account

--- assembly/neardev/main.ts
+++ assembly/neardev/main.ts
@@ -9,13 +9,6 @@ import {TimeArgs,NearArgs} from './model'
 
 const neartime_slow='neartime_slow.testnet';
 
-export class Checkout{
-	_expires:u64;
-	_index:u64;
-	to:string;
-	_amount:u128;
-}
-
 export class Saving{
 	release_time:u64;
 	amount:u64;
@@ -33,7 +26,7 @@ export class TimeApi{
 export class NEARAPI{
 	send(to: string,_amount:u128):ContractPromise{
 		//let u_amt=u128.from(amount);
-		let promise=ContractPromise.create(to,'',new Uint8Array(0),10000000000000,_amount);
+		let promise=ContractPromise.create(to,'',[],10000000000000,_amount);
 		return promise;	
 	}
 }
@@ -47,16 +40,15 @@ const amount=new PersistentMap<u64, u128>("a: ");
 const title=new PersistentMap<u64, string>("d: ");
 const saver=new PersistentMap<u64,string>("u: ");
 
-storage.set("count",0);
 
 export function init(owner: string): void{
 	logging.log("owner:  "+owner);
 	storage.set("owner",context.sender);	
 }
 
-export function NewSaving(_title: string,_target:u128,_expires:u64): bool
+/*export function NewSaving(title: string,_target:u128,_expires:u64): bool
 {
-	let count=storage.getSome<u64>("count");
+	u64 count=storage.get("count");
 	//u64 amt=context.attachedDeposit();
 	target.set(count,_target);
 	expires.set(count,_expires);
@@ -64,102 +56,71 @@ export function NewSaving(_title: string,_target:u128,_expires:u64): bool
 	amount.set(count,context.attachedDeposit);
 	title.set(count,_title);
 	let c=count+1
-	storage.set<u64>("count",c);
+	storage.set("count",c);
 	return true;
-}
+}*/
 
 export function Deposit(_index:u64): bool
 {
-	let amt=amount.getSome(_index);
+	let amt=amount.get(_index);
 	amount.set(_index,context.attachedDeposit+u128.from(amt));
 	return true;
 }
 
 export function Extend(_index:u64,_expires:u64): bool
 {
-	assert(context.sender==saver.getSome(_index));
-	assert(expires.getSome(_index)<_expires);
+	assert(context.sender==saver.get(_index));
+	assert(expires.get(_index)<_expires);
 	expires.set(_index,_expires);
 	return true;
 }
 
 export function Increase(_index:u64,_target:u128): bool
 {
-	assert(context.sender==saver.getSome(_index));
-	assert(target.getSome(_index)<_target);
+	assert(context.sender==saver.get(_index));
+	assert(target.get(_index)<_target);
 	target.set(_index,_target);
 	return true;
 }
 
 export function Withdraw(_index:u64,_amount:u128,to:string): bool
 {
-	assert(context.sender==saver.getSome(_index));
-	let amt=amount.getSome(_index);
-	let tgt=target.getSome(_index);
-	if(tgt>u128.from(0))
+	assert(context.sender==saver.get(_index));
+	let amt=u128.from(amount.get(_index));
+	if(amt>u128.from(0))
 	{
-		assert(amt>=tgt);
+		assert(amt>=u128.from(target.get(_index)));
 		_payout(_index,to,_amount);	
 		return true;
 	}
-	else{
-		let _expires=expires.getSome(_index);
-		let timeapi=new TimeApi();
-		let ts=timeapi.getDate();
-		let itemArgs: Checkout={"_expires":_expires,"_index":_index,"to":to,"_amount":_amount};
-		//let ia=encode<Uint8Array>(itemArgs);
-		let cbPromise=ts.then(
-			context.contractName,
-			"_checkTime",
-			itemArgs.encode(),
-			2
-		);
-		cbPromise.returnAsResult();
+	else if(_getTime()>expires.get(_index))
+	{
+		_payout(_index,to,_amount);	
+		return true;
 	}
 	return false;
 }
 
 function _payout(_index:u64,to:string,amt:u128):void
 {
-	let nearapi=new NEARAPI();
+	/*let nearapi=new NEARAPI();
 	let promise=nearapi.send(to,amt);
-	promise.returnAsResult();
+	promise.returnAsResult();*/
 }
 
-function _checkTime(_expires:u64,_index:u64,to:string,_amount:u128):u64
+function _getTime():u64
 {
-	let results=ContractPromise.getResults();
-	let _time=results[0];
-	if(_time.status==1){
-		let u64time=decode<u64>(_time.buffer);
-		logging.log("time "+u64time);
-		if(u64time<_expires)
-		{
-			logging.log("Payout after "+u64time.toString()+" of "+_expires.toString());
-			_payout(_index,to,_amount);		
-		}	
-	}
-}
-
-/*function _getTime():u64
-{
-	let timeapi=new TimeApi();
- 	let ts=timeapi.getDate();
- 	let res=ContractPromise.getResults();
- 	if(res[0].success)
- 	{
-		return <u64>res[0].buffer; 	
- 	}
- 	//ts.returnAsResult();
+	/*let timeapi=new TimeApi();
+	let ts=timeapi.getDate();
 	//let ts=await timestamp;
 	let results=ts.getResults();
 	if(results[0].success)
 	{
 		//convert to u64
 		return <u64>results[0].buffer;
-	}
+	}*/
 	return 9007199254740991;
-}*/
+}
 
 function _norm(amt:u64): u64
 {
